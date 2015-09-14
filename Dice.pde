edf8a1e7cf9421bfa14fe09e77dd96e6b38b1fd1
unknown <kehuang6@697-334-Harlond.sf.k12.internal>
2015-09-14 11:44:29 -0700

    multiple dices

--- Dice.pde
+++ Dice.pde
@@ -3,15 +3,20 @@ int num;
 
 void setup()
 {
-	size(300,300);
+	size(700,455);
 	noLoop();
-    one = new Die(50,50);
+    
 }
 void draw()
 {
-	one.show();
-	one.roll();
-	
+	for (int y = 10; y<400; y+= 55){
+		for (int x = 10; x<400; x+= 55){
+			Die one = new Die(x,y);
+			one.show();
+			one.roll();
+		}
+	}
+
 }
 void mousePressed()
 {
@@ -29,10 +34,45 @@ class Die //models one single dice cube
 	void roll()
 	{	
 		int rectSize = 50;
-		num = 1;
+		num = (int)(Math.random()*6)+1;
 		if (num == 1){
 			fill(0);
 			ellipse(myX+rectSize/2,myY+rectSize/2,5,5);
+		}
+		else if (num == 2){
+			fill(0);
+			ellipse(myX+rectSize/4,myY+rectSize/4,5,5);
+			ellipse(myX+3*rectSize/4,myY+3*rectSize/4,5,5);
+		}
+		else if (num == 3){
+			fill(0);
+			ellipse(myX+rectSize/2,myY+rectSize/4,5,5);
+			ellipse(myX+rectSize/2,myY+rectSize/2,5,5);
+			ellipse(myX+rectSize/2,myY+3*rectSize/4,5,5);
+		}
+		else if (num == 4){
+			fill(0);
+			ellipse(myX+rectSize/4,myY+rectSize/4,5,5);
+			ellipse(myX+3*rectSize/4,myY+3*rectSize/4,5,5);
+			ellipse(myX+rectSize/4,myY+3*rectSize/4,5,5);
+			ellipse(myX+3*rectSize/4,myY+rectSize/4,5,5);
+		}
+		else if (num == 5){
+			fill(0);
+			ellipse(myX+rectSize/4,myY+rectSize/4,5,5);
+			ellipse(myX+3*rectSize/4,myY+3*rectSize/4,5,5);
+			ellipse(myX+rectSize/4,myY+3*rectSize/4,5,5);
+			ellipse(myX+3*rectSize/4,myY+rectSize/4,5,5);
+			ellipse(myX+rectSize/2,myY+rectSize/2,5,5);
+		}
+		else if (num == 6){
+			fill(0);
+			ellipse(myX+rectSize/4,myY+rectSize/4,5,5);
+			ellipse(myX+3*rectSize/4,myY+3*rectSize/4,5,5);
+			ellipse(myX+rectSize/4,myY+3*rectSize/4,5,5);
+			ellipse(myX+3*rectSize/4,myY+rectSize/4,5,5);
+			ellipse(myX+rectSize/4,myY+2*rectSize/4,5,5);
+			ellipse(myX+3*rectSize/4,myY+2*rectSize/4,5,5);
 		}
 	}
 	void show()
--- build-tmp/source/Dice.java
+++ build-tmp/source/Dice.java
@@ -19,15 +19,20 @@ int num;
 
 public void setup()
 {
-	size(300,300);
+	size(700,455);
 	noLoop();
-    one = new Die(50,50);
+    
 }
 public void draw()
 {
-	one.show();
-	one.roll();
-	
+	for (int y = 10; y<400; y+= 55){
+		for (int x = 10; x<400; x+= 55){
+			Die one = new Die(x,y);
+			one.show();
+			one.roll();
+		}
+	}
+
 }
 public void mousePressed()
 {
@@ -45,10 +50,45 @@ class Die //models one single dice cube
 	public void roll()
 	{	
 		int rectSize = 50;
-		num = 1;
+		num = (int)(Math.random()*6)+1;
 		if (num == 1){
 			fill(0);
 			ellipse(myX+rectSize/2,myY+rectSize/2,5,5);
+		}
+		else if (num == 2){
+			fill(0);
+			ellipse(myX+rectSize/4,myY+rectSize/4,5,5);
+			ellipse(myX+3*rectSize/4,myY+3*rectSize/4,5,5);
+		}
+		else if (num == 3){
+			fill(0);
+			ellipse(myX+rectSize/2,myY+rectSize/4,5,5);
+			ellipse(myX+rectSize/2,myY+rectSize/2,5,5);
+			ellipse(myX+rectSize/2,myY+3*rectSize/4,5,5);
+		}
+		else if (num == 4){
+			fill(0);
+			ellipse(myX+rectSize/4,myY+rectSize/4,5,5);
+			ellipse(myX+3*rectSize/4,myY+3*rectSize/4,5,5);
+			ellipse(myX+rectSize/4,myY+3*rectSize/4,5,5);
+			ellipse(myX+3*rectSize/4,myY+rectSize/4,5,5);
+		}
+		else if (num == 5){
+			fill(0);
+			ellipse(myX+rectSize/4,myY+rectSize/4,5,5);
+			ellipse(myX+3*rectSize/4,myY+3*rectSize/4,5,5);
+			ellipse(myX+rectSize/4,myY+3*rectSize/4,5,5);
+			ellipse(myX+3*rectSize/4,myY+rectSize/4,5,5);
+			ellipse(myX+rectSize/2,myY+rectSize/2,5,5);
+		}
+		else if (num == 6){
+			fill(0);
+			ellipse(myX+rectSize/4,myY+rectSize/4,5,5);
+			ellipse(myX+3*rectSize/4,myY+3*rectSize/4,5,5);
+			ellipse(myX+rectSize/4,myY+3*rectSize/4,5,5);
+			ellipse(myX+3*rectSize/4,myY+rectSize/4,5,5);
+			ellipse(myX+rectSize/4,myY+2*rectSize/4,5,5);
+			ellipse(myX+3*rectSize/4,myY+2*rectSize/4,5,5);
 		}
 	}
 	public void show()
